8736bd26c807fbf4c87e8503aae1429f25013c6b
Avraaj Matharu <avr99j@gmail.com>
2024-03-14 20:30:43 +0000

    imported react-chatbot-kit library, added the classes ActionProvider and MessagParser along with the config class to render the prompt text

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import BottomNav from "./components/bottomNav/BottomNav";
 import Header from "./components/header/Header";
 import BotContainer from "./components/botContainer/BotContainer";
+import Text from "./components/text/Text"
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <BotContainer />
+      <Text />
     </div>
   );
 }
--- src/components/text/Text.jsx
+++ src/components/text/Text.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import BotContainer from './BotContainer';
+import BotContainer from '../botContainer/BotContainer';
 import './text.css';
+import { useState } from 'react';
+import Chatbot from 'react-chatbot-kit'
+import config from './config'
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
 
 function Text() {
+
+
     return (
-        <div>
+        <div className="container">
+            <Chatbot
+                config={config}
+                messageParser={MessageParser}
+                actionProvider={ActionProvider}
+            />
 
         </div>
     )
