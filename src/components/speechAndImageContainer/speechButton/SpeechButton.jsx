92fd3611ed70928ecb60fc7d406f3b183fb821e2
Selene <149156667+joanapq@users.noreply.github.com>
2024-03-14 20:47:07 +0000

    added cards to home layout

---
+++ src/components/speechAndImageContainer/speechButton/SpeechButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./speechButton.css";
+
+function SpeechButton() {
+  return (
+        <div className="col-sm-6 col-md-4">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title buttonTitle">Click to generate speech</h5>
+                 </div>
+            </div>
+        </div>  
+  );
+}
+
+export default SpeechButton;
