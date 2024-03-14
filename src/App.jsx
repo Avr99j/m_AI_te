fe5d5229723fa4d38512ea738a5891151ab1d6fd
Selene <149156667+joanapq@users.noreply.github.com>
2024-03-14 20:07:58 +0000

    amended folder structure to make space for pages which will include the different components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
-import BottomNav from "./components/bottomNav/BottomNav";
-import Header from "./components/header/Header";
-import BotContainer from "./components/botContainer/BotContainer";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import AskAI from './pages/AskAI';
+import TextToSpeech from './pages/TextToSpeech';
+import ImageGenerator from './pages/ImageGenerator';
+import './App.css'
+// import BottomNav from "./components/bottomNav/BottomNav";
+// import Header from "./components/header/Header";
+// import BotContainer from "./components/botContainer/BotContainer";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <BotContainer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="askai/*" element={<AskAI />} />
+        <Route path="texttospeach/*" element={<TextToSpeech />} />
+        <Route path="imagegenerator/*" element={<ImageGenerator />} />
+      </Routes>
+    </Router>
   );
 }
 
