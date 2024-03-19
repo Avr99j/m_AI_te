f6399220655b48456fa52735c93a4defa19263f8
Selene <149156667+joanapq@users.noreply.github.com>
2024-03-19 20:09:07 +0000

    added chat history function

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,20 +1,29 @@
-import React from "react";
-import Header from "../../components/header/header";
-import BotContainer from "../../components/botContainer/botContainer";
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
+import BotContainer from "../../components/botContainer/BotContainer";
 import SpeechToTextBtn from "../../components/speechToTextBtn/SpeechToTextBtn";
 import GeneratePicsBtn from "../../components/generatePicsBtn/GeneratePicsBtn";
 import ChatHistory from "../../components/chatHistory/ChatHistory";
 import "../home/home.css";
 
 function Home() {
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const handleReceiveMessage = (message) => {
+    setChatHistory([...chatHistory, { type: "user", message }]);
+  };
+
   return (
     <div className="wrapper">
       <Header className="header" />
       <div className="content">
-        <BotContainer className="bot-container" />
+        <BotContainer onReceiveMessage={handleReceiveMessage} />
         <SpeechToTextBtn className="speech-to-text-btn" />
         <GeneratePicsBtn className="generate-pics-container" />
-        <ChatHistory className="chat-history" />
+        <ChatHistory
+          className="chat-history"
+          chatHistory={chatHistory}
+        />
       </div>
     </div>
   );
