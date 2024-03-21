6cb81f627c216bfb878a386fec604e8e60c0c585
swathi-mungala <mswathi584@gmail.com>
2024-03-21 21:46:54 +0000

    Added avatar images

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../footer/Footer.css';
+// import '../../assets/images/*';
 
 const Footer = () => {
     return (
@@ -9,19 +10,19 @@ const Footer = () => {
                 <div className="avatar-div">
                     <p>Check out the team on Github...</p>
                     <a href="https://github.com/jpquental90" target="_blank">
-                    <img src="assets/images/avatar_1.png" alt="avatar icon" width="50px" class="avatar-icon"/>
+                    <img src = "../../src/assets/images/avatar_1.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/chrisS-88" target="_blank">
-                    <img src="assets/images/avatar_2.png" alt="avatar icon" width="50px" class="avatar-icon"/>
+                    <img src="../src/assets/images/avatar_2.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/Avr99j" target="_blank">
-                    <img src="assets/images/avatar_4.png" alt="avatar icon" width="50px" class="avatar-icon"/>
+                    <img src="../../../src/assets/images/avatar_4.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/TheoBurton" target="_blank">
-                    <img src="assets/images/avatar_3.png" alt="avatar icon" width="50px" class="avatar-icon"/>
+                    <img src="../../../src/assets/images/avatar_4.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/swathi-mungala" target="_blank">
-                    <img src="assets/images/avatar_3.png" alt="avatar icon" width="50px" class="avatar-icon"/>
+                    <img src="../src/assets/images/avatar_2.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                 </div>
             </div>
