43f4821747cf7369612800bca959a484193d4bc0
swathi-mungala <mswathi584@gmail.com>
2024-03-22 18:56:56 +0000

    Added footer styling changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => {
             <p className="rights">&copy; 2024 My Website. All Rights Reserved.</p>
                 <div className="avatar-div">
                     <p>Check out the team on Github...</p>
+                    <div>
                     <a href="https://github.com/jpquental90" target="_blank">
                     <img src = "../../src/assets/images/avatar_1.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
@@ -15,14 +16,19 @@ const Footer = () => {
                     <img src="../src/assets/images/avatar_2.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/Avr99j" target="_blank">
-                    <img src="../../../src/assets/images/avatar_4.png" alt="avatar icon" width="50px" className="avatar-icon"/>
+                    <img src="../../../src/assets/images/avatar_3.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/TheoBurton" target="_blank">
                     <img src="../../../src/assets/images/avatar_4.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
                     <a href="https://github.com/swathi-mungala" target="_blank">
+                    <img src="../src/assets/images/avatar_1.png" alt="avatar icon" width="50px" className="avatar-icon"/>
+                    </a>
+                    <a href="https://github.com/Terr0y" target="_blank">
                     <img src="../src/assets/images/avatar_2.png" alt="avatar icon" width="50px" className="avatar-icon"/>
                     </a>
+                    </div>
+                    
                 </div>
             </div>
         </footer>
